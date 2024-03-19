60beae6959a491dfcd85dce25406369eb9900043
LegalmenteEmanuel <emanuel070801@gmail.com>
2024-03-18 21:07:42 -0600

    feat: ya se puede enviar mensajes en la sala de chat

--- socket-chat/src/chat/chat.gateway.ts
+++ socket-chat/src/chat/chat.gateway.ts
@@ -1,4 +1,4 @@
-import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { OnModuleInit } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { Server, Socket } from 'socket.io';
@@ -13,13 +13,56 @@ export class ChatGateway implements OnModuleInit{
 
   onModuleInit() {
     this.server.on('connection', (socket:Socket) => {
-      console.log('Cliente Conectado: ', socket.id);
 
+      const {name,token} = socket.handshake.auth;
+
+      console.log({name,token});
+      if(!name){
+        socket.disconnect();
+        return;
+      }
+
+      this.chatService.onClientConnected({id:socket.id, name:name});
+
+      //Mensaje de bienvenida
+      socket.emit('welcome-message', 'Bienvenido al servidor')
+
+      // Listado de clientes conectados
+      this.server.emit('on-clients-changed', this.chatService.getClients());
+      
+      
       socket.on('disconnect', () => {
-        console.log('Cliente desconectado: ', socket.id);
-        
+        //console.log('Cliente desconectado: ', socket.id);
+        this.chatService.onClientDisconnected(socket.id);
+        this.server.emit('on-clients-changed', this.chatService.getClients());
       })
       
     })
   }
+
+  @SubscribeMessage('send-message')
+  handleMessage(
+    @MessageBody() message:string,
+    @ConnectedSocket() client: Socket,
+  ){
+    const {name, token} = client.handshake.auth;
+    console.log({name,message});
+    
+
+    if(!message) {
+      return;
+    }
+    this.server.emit(
+      'on-message',
+      {
+        userId: client.id,
+        message: message,
+        name:name
+      }
+    )
+  }
+
+
+
+
 }
